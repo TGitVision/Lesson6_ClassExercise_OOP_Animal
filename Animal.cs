ff8b0bc773f09c7eece3c9e0b201c61679f903f1
TGitVision <60406685+TGitVision@users.noreply.github.com>
2020-07-22 01:40:16 -0400

    Add project files.

---
+++ Animal.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lesson6_ClassExercise_OOP_Animal
+{
+    class Animal
+    {
+
+        private int legs { get; set; }
+        private int arms { get; set; }
+        private string type { get; set; }
+        public string name { get; private set; }
+        private bool isDangerous { get; set; }
+
+        public Animal(int legs, int arms, string type, string name, bool isDangerous)
+        {
+            // Note: "this" references the Animal class. Used with a period (.) 
+            // the Property is referenced and doesn't get mixed up with the constructor 
+            // arg with the same name.
+
+            this.legs = legs;
+            this.arms = arms;
+            this.type = type;
+            this.name = name;
+            this.isDangerous = isDangerous;
+
+        }
+        public void Walk()
+        {
+            Console.WriteLine($"{this.name} just walked across the floor.");
+        }
+
+        public string GetNumberOfLegsStatementForAnimal3()
+        {
+            string strReturn = string.Empty;
+
+            strReturn = $"{this.name} has {this.legs} legs.";
+
+            return strReturn;
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Lesson6_ClassExercise_OOP_Animal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Lesson6_ClassExercise_OOP_Animal!");
+
+            // Declare and Instantiate the Animal class with different arguments...added by th 07042020
+            Animal animal1 = new Animal(4, 0, "Dog", "Max", true);
+            Animal animal2 = new Animal(2, 2, "Monkey", "Tim", false);
+            Animal animal3 = new Animal(8, 0, "Spider", "Friendly the Spider", true);
+
+            Console.WriteLine($"Animal Names: {animal1.name} , {animal2.name} , {animal3.name}");
+
+            animal3.Walk();
+
+            Console.WriteLine(animal3.GetNumberOfLegsStatementForAnimal3());
+
+        }
+    }
+}
